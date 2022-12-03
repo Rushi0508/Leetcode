1fe52011a9b561fca3e52c2cb85dc4d4f602f632
Rushi Gandhi <88783422+Rushi0508@users.noreply.github.com>
2022-12-03 09:47:30 +0530

    Added solution - LeetHub

---
+++ Find length of Loop - GFG/find-length-of-loop.cpp
@@ -0,0 +1,106 @@
+//{ Driver Code Starts
+// driver code
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void loopHere(Node* head, Node* tail, int position)
+{
+    if(position==0) return;
+    
+    Node* walk = head;
+    for(int i=1; i<position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+int countNodesinLoop(Node* head);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n, num;
+        cin>>n;
+        
+        Node *head, *tail;
+        cin>> num;
+        head = tail = new Node(num);
+        
+        for(int i=0 ; i<n-1 ; i++)
+        {
+            cin>> num;
+            tail->next = new Node(num);
+            tail = tail->next;
+        }
+        
+        int pos;
+        cin>> pos;
+        loopHere(head,tail,pos);
+        
+        cout<< countNodesinLoop(head) << endl;
+    }
+	return 0;
+}
+
+// } Driver Code Ends
+
+
+/*
+
+struct Node {
+    int data;
+    struct Node *next;
+    Node(int x) {
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+//Function to find the length of a loop in the linked list.
+int countNodes(struct Node* n)
+{
+    int res = 1;
+    struct Node* temp = n;
+    while (temp->next != n) {
+        res++;
+        temp = temp->next;
+    }
+    return res;
+}
+int countNodesinLoop(struct Node *list)
+{
+    // Code here
+    struct Node *slow_p = list; struct Node *fast_p = list;
+ 
+    while (slow_p && fast_p && fast_p->next) {
+        slow_p = slow_p->next;
+        fast_p = fast_p->next->next;
+ 
+        /* If slow_p and fast_p meet at
+        some point then there is a loop */
+        if (slow_p == fast_p)
+            return countNodes(slow_p);
+    }
+ 
+    /* Return 0 to indicate that
+       there is no loop*/
+    return 0;
+    
+}
