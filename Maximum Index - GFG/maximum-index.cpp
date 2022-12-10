37a09f0b4ee002d1053d5319ee976c4230d32488
Rushi Gandhi <88783422+Rushi0508@users.noreply.github.com>
2022-12-10 19:55:55 +0530

    Added solution - LeetHub

---
+++ Maximum Index - GFG/maximum-index.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+    public:
+        
+    // A[]: input array
+    // N: size of array
+    // Function to find the maximum index difference.
+    int maxIndexDiff(int A[], int N) 
+    { 
+        // Your code here
+        int low=0,high=N-1,total=0,diff;
+       while(high>=low)
+       {
+           if(A[high]>=A[low])
+           {
+               diff=high-low;
+               total=max(total,diff);
+               low++;
+               high=N-1;
+           }
+           else
+           high--;
+           
+       }
+       return total;}
+};
+
+//{ Driver Code Starts.
+  
+/* Driver program to test above functions */
+int main() 
+{
+    int T;
+    //testcases
+    cin>>T;
+    while(T--){
+        int num;
+        //size of array
+        cin>>num;
+        int arr[num];
+        
+        //inserting elements
+        for (int i = 0; i<num; i++)
+            cin>>arr[i];
+        Solution ob;
+        
+        //calling maxIndexDiff() function
+        cout<<ob.maxIndexDiff(arr, num)<<endl;    
+        
+    }
+    return 0;
+} 
+// } Driver Code Ends
