76b11203278d7d1b289248e1d2de632569de95e4
Rushi Gandhi <88783422+Rushi0508@users.noreply.github.com>
2023-01-01 13:17:21 +0530

    Added solution - LeetHub

---
+++ Find n/k th node in Linked list - GFG/find-nk-th-node-in-linked-list.cpp
@@ -0,0 +1,95 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+int fractional_node(struct Node* head,int k);
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        Node* ptr,*start = NULL,*ptr1;
+        int n,i;
+        cin>>n;
+        
+        for(i=0;i<n;i++)
+        {
+            int value;
+            cin>>value;
+            
+            ptr=new Node(value);
+            
+            if(start==NULL)
+            {
+                start=ptr;
+                ptr1=ptr;
+            }
+            else
+            {
+                ptr1->next=ptr;
+                ptr1=ptr1->next;
+            }
+        }
+        ptr1->next=NULL;
+        int k;
+        cin>>k;
+        int p = fractional_node(start,k);
+        cout<<p<<endl;
+    }
+}
+
+// } Driver Code Ends
+
+
+/*The structure of the node is
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+*/
+int fractional_node(struct Node *head, int k)
+{
+    // your code here
+    Node* temp = head;
+    int n = 1;
+    while(temp->next!=NULL){
+        n++;
+        temp = temp->next;
+    }
+    int count=1;
+    Node* curr=head;
+    int ans;
+    while(curr!=NULL){
+        if(count==n/k){
+            ans=  curr->data;
+            break;
+        }
+        curr = curr->next;
+        count++;
+    }
+    if(n%k!=0){
+        return curr->next->data;
+    }
+    return ans;
+}
