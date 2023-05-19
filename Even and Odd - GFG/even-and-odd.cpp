371c971d59a0ae21043627656ee8c7ea4fccea1d
Rushi Gandhi <88783422+Rushi0508@users.noreply.github.com>
2023-05-20 00:58:14 +0530

    Added solution - LeetHub

---
+++ Even and Odd - GFG/even-and-odd.cpp
@@ -0,0 +1,81 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    void reArrange(int arr[], int n) {
+        // code here
+        int odd=1;
+        int even=0;
+        while(even<n and odd<n){
+            if(arr[odd]&1){
+                odd+=2;
+            }else if(!(arr[even] & 1)){
+                even+=2;
+            }else{
+                swap(arr[odd],arr[even]);
+            }
+        }
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int check(int arr[], int n)
+{
+    int flag = 1;
+    int c=0, d=0;
+    for(int i=0; i<n; i++)
+    {
+        if(i%2==0)
+        {
+            if(arr[i]%2)
+            {
+                flag = 0;
+                break;
+            }
+            else
+                c++;
+        }
+        else
+        {
+            if(arr[i]%2==0)
+            {
+                flag = 0;
+                break;
+            }
+            else
+                d++;
+        }
+    }
+    if(c!=d)
+        flag = 0;
+        
+    return flag;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int arr[N];
+        for(int i=0; i<N; i++)
+            cin>>arr[i];
+
+        Solution ob;
+        ob.reArrange(arr,N);
+        
+        cout<<check(arr,N)<<endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
